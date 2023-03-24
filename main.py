521506e265da095c30280369e0bd12862bba606a
Kyle Johnson <miningape@gmail.com>
2023-03-24 01:29:06 +0100

    login widget

--- main.py
+++ main.py
@@ -1,31 +1,87 @@
 import sys
 import random
+from typing import List, Optional
+from dataclasses import dataclass
+
 from PySide6 import QtCore, QtWidgets, QtGui
 
-class MyWidget(QtWidgets.QWidget):
+@dataclass
+class Credentials:
+    username: str
+    pasword: str
+
+class LoginForm(QtWidgets.QWidget):
+    onLogin = QtCore.Signal(Credentials)
+    username: QtWidgets.QLineEdit
+    password: QtWidgets.QLineEdit
+    button: QtWidgets.QPushButton
+
+    def __init__(self, parent: Optional[QtWidgets.QWidget] = None):
+        super(LoginForm, self).__init__(parent)
+
+        self.username = QtWidgets.QLineEdit()
+        self.password = QtWidgets.QLineEdit()
+        self.password.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
+
+        self.button = QtWidgets.QPushButton("Log In.")
+        self.button.clicked.connect(self.loginUser)
+
+        self.formLayout = QtWidgets.QFormLayout()
+        self.formLayout.addRow("Username:", self.username)
+        self.formLayout.addRow("Password:", self.password)
+        self.formLayout.setFormAlignment(QtCore.Qt.AlignmentFlag.AlignCenter | QtCore.Qt.AlignmentFlag.AlignBottom)
+
+        self.layout = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.Direction.Down, self)
+        self.layout.addLayout(self.formLayout)
+        self.layout.addWidget(self.button, alignment=QtCore.Qt.AlignmentFlag.AlignCenter | QtCore.Qt.AlignmentFlag.AlignTop)
+
+        # self.setContentsMargins(100, 100, 100, 100)
+        # self.setFrameStyle(QtWidgets.QFrame.Shadow.Sunken | QtWidgets.QFrame.Shape.StyledPanel)
+        # self.setLineWidth(1)
+
+        # self.setFrameStyle("margin: 5px;")
+        # self.setMaximumHeight(self.height() / 2)
+        # self.setMargin(self.width() / 2)
+        # self.resize(self.sizeHint())
+        # self.setFrameShape()
+        # self.setFrameStyle(QtCore. QFrame::Panel | QFrame::Raised);
+
+    def loginUser(self):
+        self.onLogin.emit(
+            Credentials(
+                username=self.username.text(),
+                pasword=self.password.text()
+            )
+        )
+
+
+class Core (QtWidgets.QWidget):
+    logout = QtCore.Signal()
+
+
+class MyApp (QtWidgets.QMainWindow):
+    credentials: Optional[Credentials] = None
+    login: LoginForm
+
     def __init__(self):
-        super().__init__()
+        super(MyApp, self).__init__()
+        self.setWindowTitle("Challenge 1 - Workflow")
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+        self.login = LoginForm()
+        self.login.onLogin.connect(self.setCredentials)
+        self.setCentralWidget(self.login)
 
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
+    @QtCore.Slot(Credentials)
+    def setCredentials(self, credentials: Credentials):
+        self.credentials = credentials
+        print(self.credentials)
+        self.login.deleteLater()
 
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-
-        self.button.clicked.connect(self.magic)
-
-    @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
 
 if __name__ == "__main__":
-    # app = QtWidgets.QApplication([])
+    app = QtWidgets.QApplication([])
 
-    widget = MyWidget()
+    widget = MyApp()
     widget.resize(800, 600)
     widget.show()
 
